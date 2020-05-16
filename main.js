373bfcc79d6433e67b8ef48737ed482bf828992f
Libia A. Pérez B <60617404+alexadictiva@users.noreply.github.com>
2020-05-16 08:49:48 -0300

    intento 2

---
+++ main.js
@@ -0,0 +1,24 @@
+/* let boton = document.getElementById("mostrar-menu");
+boton.addEventListener("click", mostrarMenu);
+
+function mostrarMenu() {
+  boton.style.display = "none";
+}
+ */
+/* const button = document.querySelector(".mostrar-menu");
+const icon = document.querySelector(".fa-bars");
+
+icon.style.transform = "300ms ease-on-out";
+let open = false;
+
+button.addEventListener("click", () => {
+  if (!open) {
+    icon.style.transform = rotate(-90deg);
+    setTimeout(() => icon.setAttribute("class", "fas fa-times icon"), 120);
+    open = true;
+  } else {
+    icon.style.transform = rotate(0deg);
+    setTimeout(() => icon.setAttribute("class", "fas fa-bars"), 120);
+    open = false;
+  }
+}); */
